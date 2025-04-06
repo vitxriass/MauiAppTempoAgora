33b83d9833142b0a143568f32ebd8728e2b576eb
vitxriass <vitoriasilvasan23@gmail.com>
2025-04-06 16:32:26 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    }
+                    else
+                    {
+
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+
+}
